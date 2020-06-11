948a9187a601bb658afe693616f7b9c5e3717da4
David Boxwell <davidboxwell@ymail.com>
2020-06-11 18:36:09 +0100

    Finish logger

---
+++ LeadEngine/src/LeadEngine/log.cpp
@@ -0,0 +1,22 @@
+#include "le_pch.h"
+
+#include "log.h"
+
+#include "spdlog/sinks/stdout_color_sinks.h"
+
+namespace le
+{
+	logger Log::coreLogger;
+	logger Log::clientLogger;
+
+	void Log::init()
+	{
+		spdlog::set_pattern("%^[%T] %n: %v%$");
+
+		coreLogger = spdlog::stdout_color_mt("LEADENGINE");
+		coreLogger->set_level(spdlog::level::trace);
+
+		clientLogger = spdlog::stdout_color_mt("APP");
+		clientLogger->set_level(spdlog::level::trace);
+	}
+}
--- LeadEngine/src/LeadEngine/log.h
+++ LeadEngine/src/LeadEngine/log.h
@@ -1,10 +1,13 @@
 #pragma once
 
+#include <memory>
+
 #include "core.h"
 #include "spdlog/spdlog.h"
+#include "spdlog/fmt/ostr.h"
 
 namespace le
-{
+{ 
 	typedef std::shared_ptr<spdlog::logger> logger;
 
 	class LE_API Log
@@ -13,13 +16,30 @@ namespace le
 		static logger coreLogger;
 		static logger clientLogger;
 	public:
-		Log();
-		~Log();
+		static void init();
 
 		inline static logger& getCoreLogger()
 		{
 			return coreLogger;
 		}
+		inline static logger& getClientLogger()
+		{
+			return clientLogger;
+		}
 	
 	};
 }
+
+/* Core log macros. */
+#define LE_CORE_TRACE(...) ::le::Log::getCoreLogger()->trace(__VA_ARGS__)
+#define LE_CORE_INFO(...) ::le::Log::getCoreLogger()->info(__VA_ARGS__)
+#define LE_CORE_WARN(...) ::le::Log::getCoreLogger()->warn(__VA_ARGS__)
+#define LE_CORE_ERROR(...) ::le::Log::getCoreLogger()->error(__VA_ARGS__)
+#define LE_CORE_FATAL(...) ::le::Log::getCoreLogger()->fatal(__VA_ARGS__)
+
+/* Client log macros. */
+#define LE_TRACE(...) ::le::Log::getClientLogger()->trace(__VA_ARGS__)
+#define LE_INFO(...) ::le::Log::getClientLogger()->info(__VA_ARGS__)
+#define LE_WARN(...) ::le::Log::getClientLogger()->warn(__VA_ARGS__)
+#define LE_ERROR(...) ::le::Log::getClientLogger()->error(__VA_ARGS__)
+#define LE_FATAL(...) ::le::Log::getClientLogger()->fatal(__VA_ARGS__)
